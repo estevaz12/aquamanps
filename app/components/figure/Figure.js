e540f9775c73daac022110d3bd680c4a823f5218
Esteban Vazquez <esteban.vazquez@techbrainscorp.com>
2023-06-20 17:06:17 -0300

    finished before and after effect

--- app/components/figure/Figure.js
+++ app/components/figure/Figure.js
@@ -1,20 +1,37 @@
+'use client';
+
 import styles from './Figure.module.css';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function BeforeAfterFig({ className }) {
+  const [sliderVal, setSliderVal] = useState(50);
+
+  function handleChange(e) {
+    setSliderVal(e.target.value);
+  }
+
   return (
     <figure className={className}>
-      <Image
-        src='/images/pool2.jpg'
-        alt='Pool'
-        width={320}
-        height={320}
-        loading='lazy'
-        className={styles.img}
-      />
+      <div className={`${styles.imgContainer} ${styles.after}`}>
+        <Image src='/images/pool2.jpg' alt='Pool' fill={true} />
+        <div className={`${styles.before}`} style={{ width: `${sliderVal}%` }}>
+          <Image src='/images/pool2.jpg' alt='Pool' fill={true} />
+        </div>
+        <input
+          type='range'
+          min='1'
+          max='100'
+          value={sliderVal}
+          onChange={handleChange}
+          className={styles.slider}
+          name='slider'
+        />
+      </div>
+
       <figcaption>
         <small>
-          <i>Drag the slider to see before and after</i>
+          <i>Drag to see before and after</i>
         </small>
       </figcaption>
     </figure>
