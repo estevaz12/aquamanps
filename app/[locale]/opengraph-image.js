82c01bbc75a37da993b95728c5eb1ec59b8e448c
Esteban Vazquez <esteban.vazquez@techbrainscorp.com>
2023-06-29 11:09:59 -0300

    added og image

---
+++ app/[locale]/opengraph-image.js
@@ -0,0 +1,43 @@
+import { ImageResponse } from 'next/server';
+import Hero from '../components/hero/Hero';
+
+// Route segment config
+export const runtime = 'edge';
+
+// Image metadata
+export const alt = 'Aquaman Pool Services';
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+// Font
+// const interSemiBold = fetch(
+//   new URL('./Inter-SemiBold.ttf', import.meta.url)
+// ).then((res) => res.arrayBuffer())
+
+export const contentType = 'image/png';
+
+// Image generation
+export default function Image() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <Hero />
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      // fonts: [
+      //   {
+      //     name: 'Inter',
+      //     data: await interSemiBold,
+      //     style: 'normal',
+      //     weight: 400,
+      //   },
+      // ],
+    }
+  );
+}
